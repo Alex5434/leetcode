648dac78f15c5a5b6a690bdae3c1cdb70774bcc1
Alexander <alexander@visailabs.com>
2025-10-27 19:26:50 +0530

    Solved using DFS appproach

---
+++ 200.number-of-islands.java
@@ -0,0 +1,111 @@
+/*
+ * @lc app=leetcode id=200 lang=java
+ *
+ * [200] Number of Islands
+ *
+ * https://leetcode.com/problems/number-of-islands/description/
+ *
+ * algorithms
+ * Medium (58.13%)
+ * Likes:    24387
+ * Dislikes: 594
+ * Total Accepted:    3.8M
+ * Total Submissions: 5.9M
+ * Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]'
+ *
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land) and
+ * '0's (water), return the number of islands.
+ * 
+ * An island is surrounded by water and is formed by connecting adjacent lands
+ * horizontally or vertically. You may assume all four edges of the grid are
+ * all surrounded by water.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: grid = [
+ * ⁠ ["1","1","1","1","0"],
+ * ⁠ ["1","1","0","1","0"],
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["0","0","0","0","0"]
+ * ]
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: grid = [
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["1","1","0","0","0"],
+ * ⁠ ["0","0","1","0","0"],
+ * ⁠ ["0","0","0","1","1"]
+ * ]
+ * Output: 3
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] is '0' or '1'.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public static int numIslands(char[][] grid) {
+
+        int count = 0;
+
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                if (grid[r][c] == '1') {
+                    count++;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void dfs(char[][] grid, int r, int c) {
+        // base condition
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+
+    public static void main(String[] args) {
+
+        // char grid[][] = {
+        //         { '1', '1', '1', '1', '0' },
+        //         { '1', '1', '0', '1', '0' },
+        //         { '1', '1', '0', '0', '0' },
+        //         { '0', '0', '0', '0', '0' }
+        // };
+        char grid[][] = {
+                {'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','1'}
+        };
+
+        int result  = numIslands(grid);
+
+        System.out.println("THE RESULT " + result);
+    }
+}
+// @lc code=end
