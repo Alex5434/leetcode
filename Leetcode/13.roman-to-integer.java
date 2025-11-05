8d7d8f8baaed949bcc628bb1d73169301391e737
alex <ezhil2744@gmail.com>
2025-11-05 23:34:21 +0530

    Easy 13, It works and can be improved

--- Leetcode/13.roman-to-integer.java
+++ Leetcode/13.roman-to-integer.java
@@ -16,6 +16,8 @@
  * Roman numerals are represented by seven different symbols: I, V, X, L, C, D
  * and M.
  * 
+ * {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000, "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+ * 
  * 
  * Symbol       Value
  * I             1
@@ -81,10 +83,45 @@
  */
 
 // @lc code=start
+
+import java.util.*;
+
 class Solution {
-    public int romanToInt(String s) {
-        
+    public static int romanToInt(String s) {
+        Map<String, Integer> map = Map.ofEntries(
+                Map.entry("I", 1),
+                Map.entry("V", 5),
+                Map.entry("X", 10),
+                Map.entry("L", 50),
+                Map.entry("C", 100),
+                Map.entry("D", 500),
+                Map.entry("M", 1000),
+                Map.entry("IV", 4),
+                Map.entry("IX", 9),
+                Map.entry("XL", 40),
+                Map.entry("XC", 90),
+                Map.entry("CD", 400),
+                Map.entry("CM", 900));
+
+        int runningSum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i+1 < s.length() && map.containsKey(s.charAt(i) + "" + s.charAt(i + 1))) {
+                runningSum+= map.get(s.charAt(i) + "" + s.charAt(i + 1));
+                i++;
+            } else {
+                runningSum+= map.get(s.charAt(i)+ "");
+            }
+        }
+
+        return runningSum;
+    }
+
+    public static void main(String[] args) {
+        // String s = "MCMXCIV";
+        String s = "MCMXCIV";
+        int run = romanToInt(s);
+
+        System.out.println("RUNN ::" + run);
     }
 }
 // @lc code=end
-
