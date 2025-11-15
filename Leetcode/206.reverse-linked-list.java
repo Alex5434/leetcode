b642ffa42a11750e8f31b21cfa94793f3a3745e7
alex <ezhil2744@gmail.com>
2025-11-15 22:40:14 +0530

    Compled reverse linked list

---
+++ Leetcode/206.reverse-linked-list.java
@@ -0,0 +1,115 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (79.41%)
+ * Likes:    23859
+ * Dislikes: 557
+ * Total Accepted:    5.8M
+ * Total Submissions: 7.3M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given the head of a singly linked list, reverse the list, and return the
+ * reversed list.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2]
+ * Output: [2,1]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = []
+ * Output: []
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is the range [0, 5000].
+ * -5000 <= Node.val <= 5000
+ * 
+ * 
+ * 
+ * Follow up: A linked list can be reversed either iteratively or recursively.
+ * Could you implement both?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+import java.util.*;
+class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        return prev;
+    }
+
+    // public static void sample(){
+    //     Stack<Integer> stack = new Stack<>();
+
+    //     stack.push(1);
+    //     stack.push(2);
+    //     stack.push(3);
+        
+    //     // stack.push(4);
+
+    //     System.out.println("LKK" + stack);
+        
+    //     while (stack.size() - 2 >= 0) {
+    //         int m = stack.pop();
+    //         int n = stack.pop();
+    //         System.out.println("LKK" + stack + " :: M " + m +" NN : " + n);
+    //     }
+    // }
+
+    public static void main(String[] args) {
+        sample();
+    }
+}
+// @lc code=end
+
