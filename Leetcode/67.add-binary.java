7f51ad7182ca456c1f04129ef997f1a4a7493de9
Alexander <alexander@visailabs.com>
2025-11-27 11:27:51 +0530

    Studied Merge sort and  Modulo trick for 1015

--- Leetcode/67.add-binary.java
+++ Leetcode/67.add-binary.java
@@ -36,48 +36,72 @@
 
 // @lc code=start
 class Solution {
+
+    // ⚠️ BRUTE FORCE FAILED !!! Integer limit exceeded
+    // public static String addBinary(String a, String b) {
+
+    //     if(a == "0" && b == "0"){
+    //         System.out.println("LLLL");
+    //         return "0";
+    //     }
+
+    //     int fi = 0, se = 0;
+    //     int counter = 0;
+    //     for (int i = a.length() - 1; i >= 0; i--) {
+    //         if(a.charAt(i) == '1'){
+    //             fi+= Math.pow(2, counter);
+    //         }
+
+    //         counter++;
+    //     }
+
+    //     counter = 0;
+
+    //     for (int i = b.length() - 1; i >= 0; i--) {
+    //         if(b.charAt(i) == '1'){
+    //             se+= Math.pow(2, counter);
+    //         }
+    //         counter++;
+    //     }
+
+    //     System.out.println("FIRST ::: " + fi + " :: " + se);
+
+    //     int su = fi + se;
+    //     String result = "";
+    //     while (su > 0) {
+    //         System.out.println("::::");
+    //         result = (su % 2) + result;
+    //         System.out.println("RESULT :: " + result);
+    //         su/= 2;
+    //     }
+    //     System.out.println("KKK result" + result);
+    //     return result;
+    // }
+
+    // 📝 Solved the binary sum using sum and carry method
     public static String addBinary(String a, String b) {
 
-        if(a == "0" && b == "0"){
-            System.out.println("LLLL");
-            return "0";
+        int p1 = a.length() - 1;
+        int p2 = b.length() - 1;
+
+        String re = "";
+        int carry = 0;
+
+        while(p1 >= 0 || p2 >= 0 || carry != 0){
+            int sum = carry;
+            if(p1 >= 0) sum += a.charAt(p1--) - '0';
+            if(p2 >= 0) sum += b.charAt(p2--) - '0';
+            re = sum % 2 + re;
+            carry = sum / 2;
         }
-
-        int fi = 0, se = 0;
-        int counter = 0;
-        for (int i = a.length() - 1; i >= 0; i--) {
-            if(a.charAt(i) == '1'){
-                fi+= Math.pow(2, counter);
-            }
-
-            counter++;
-        }
-
-        counter = 0;
-
-        for (int i = b.length() - 1; i >= 0; i--) {
-            if(b.charAt(i) == '1'){
-                se+= Math.pow(2, counter);
-            }
-            counter++;
-        }
-
-        System.out.println("FIRST ::: " + fi + " :: " + se);
-
-        int su = fi + se;
-        String result = "";
-        while (su > 0) {
-            System.out.println("::::");
-            result = (su % 2) + result;
-            System.out.println("RESULT :: " + result);
-            su/= 2;
-        }
-        System.out.println("KKK result" + result);
-        return result;
+        return re;
     }
 
+
+
     public static void main(String[] args) {
-        String first = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", second = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+        String first = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+        String second = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
         String re = addBinary(first, second);
 
         System.out.println("FINAL :: " + re);
