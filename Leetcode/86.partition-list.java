d84a85bdcc29bb0cd25c1f0e5fad0ad077ade7ce
Alexander <alexander@visailabs.com>
2025-12-30 12:37:48 +0530

    Solved partition list and started learninig binary search

---
+++ Leetcode/86.partition-list.java
@@ -0,0 +1,123 @@
+/*
+ * @lc app=leetcode id=86 lang=java
+ *
+ * [86] Partition List
+ *
+ * https://leetcode.com/problems/partition-list/description/
+ *
+ * algorithms
+ * Medium (55.40%)
+ * Likes:    7942
+ * Dislikes: 972
+ * Total Accepted:    853.5K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[1,4,3,2,5,2]\n3'
+ *
+ * Given the head of a linked list and a value x, partition it such that all
+ * nodes less than x come before nodes greater than or equal to x.
+ * 
+ * You should preserve the original relative order of the nodes in each of the
+ * two partitions.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,4,3,2,5,2], x = 3
+ * Output: [1,2,2,4,3,5]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [2,1], x = 2
+ * Output: [1,2]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is in the range [0, 200].
+ * -100 <= Node.val <= 100
+ * -200 <= x <= 200
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public static ListNode partition(ListNode head, int x) {
+        ListNode left = new ListNode(0);
+        ListNode right = new ListNode(0);
+
+        ListNode leftTail = left;
+        ListNode rightTail = right;
+
+        while(head != null){
+            // ListNode temp = new ListNode(head.val);
+            if(head.val < x){
+                leftTail.next = head;
+                leftTail = leftTail.next;
+            } else {
+                rightTail.next = head;
+                rightTail = rightTail.next;
+            }
+            head = head.next;
+        }
+
+        printList(left);
+        printList(right);
+
+        // leftTail.next = right.next;
+        // rightTail.next = null;
+        return left.next;
+    }
+
+    static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(2);
+        l1.next.next.next.next = new ListNode(5);
+        l1.next.next.next.next.next = new ListNode(2);
+
+        int x = 3;
+
+        ListNode re = partition(l1, x);
+
+        // printList(re);
+
+
+    }
+}
+
+// @lc code=end
+
